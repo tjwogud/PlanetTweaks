afa084ca3dd89b4e2fcbd896e49326f6352af075
tjwogud <rlatjsal78@gmail.com>
2021-11-18 06:31:02 +0900

    commit

---
+++ PlanetTweaks/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using UnityModManagerNet;
+
+namespace PlanetTweaks
+{
+    public class Settings : UnityModManager.ModSettings
+    {
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            var filepath = Path.Combine(modEntry.Path, "Settings.xml");
+            using (var writer = new StreamWriter(filepath))
+                new XmlSerializer(GetType()).Serialize(writer, this);
+        }
+
+        public string redSelected = null;
+        public string blueSelected = null;
+        public string spriteDirectory = Path.Combine(".", "Mods", "PlanetTweaks", "sprites");
+    }
+}
