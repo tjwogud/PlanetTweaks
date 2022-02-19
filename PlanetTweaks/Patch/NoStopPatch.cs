382f8e8e7955e26197d60a2691e5d042480e8b64
tjwogud <rlatjsal78@gmail.com>
2022-02-19 18:08:35 +0900

    add color select moving tile

---
+++ PlanetTweaks/Patch/NoStopPatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlanetTweaks.Patch
+{
+    public static class NoStopPatch
+    {
+        public static void Postfix(int keyCode, ref bool __result)
+        {
+            if (scrController.isGameWorld || (keyCode != 61000 && keyCode != 61008))
+                return;
+            float x = (float)Math.Round(scrController.instance.chosenplanet.transform.position.x);
+            float y = (float)Math.Round(scrController.instance.chosenplanet.transform.position.y);
+            if ((x == 3 || x == -3) && (y >= -18 && y <= -7))
+            {
+                __result = true;
+            }
+        }
+    }
+}
